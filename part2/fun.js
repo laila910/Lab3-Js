ac1ddb88f049ca290b27e8a1cd15c7c5c8e8804c
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2022-08-27 01:13:39 +0200

    continue in solve phonebook

--- part2/fun.js
+++ part2/fun.js
@@ -15,3 +15,39 @@ var average = sum / arrOfNumbers.length;
 alert(`the sum of the entered values equal to ${sum} and the average equal to ${average}`);
 
 }
+
+function phonebook(){
+var operation = prompt('please enter your operation "add" or "search"');
+var ContactsArray=[];
+if(operation=='add'){
+var contactname =String(prompt("please enter the name of the contact"));
+var phonenumber=Number(prompt('please enter the phone number of the contact'));
+var contactObj={};
+contactObj.ContactName=contactname;
+contactObj.phoneNumber=phonenumber;
+for(var property in contactObj) {
+    alert(property + "=" + contactObj[property]);
+}
+ContactsArray.push(contactObj);
+phonebook();
+}else if(operation=='search'){
+var searchContact=prompt("please enter what you search for");
+alert(ContactsArray);
+alert(ContactsArray.length);
+for(var i=0;i<ContactsArray.length;i++){
+    alert(ContactsArray[i]);
+    for(var key in ContactsArray[i]){
+        alert(ContactsArray[i].key);
+        alert(key.includes(searchContact));
+      
+        if(ContactsArray[i].key.includes(searchContact)){   
+            alert(`found your search`);  
+                alert(`the full details of your search are ${ContactsArray[i].ContactName} and ${ContactsArray[i].phoneNumber}`);
+        }   
+    }
+}
+phonebook();
+
+}
+
+}
